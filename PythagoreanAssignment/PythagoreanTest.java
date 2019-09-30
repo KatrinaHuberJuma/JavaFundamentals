d31817785675049ca7ea63ba2574c2b9b34e7327
KatrinaHuberJuma <katrina.huber@gmail.com>
2019-09-30 14:01:46 -0700

    Pythagorean Assignment

---
+++ PythagoreanAssignment/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean myHyp = new Pythagorean();
+        double hypotenuse = myHyp.calculateHypotenuse(4, 5);
+        System.out.println(hypotenuse);
+    }
+}
