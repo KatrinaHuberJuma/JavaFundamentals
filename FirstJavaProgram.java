30ea30736159676fb46e2c9f63ffbad4df80fe7d
KatrinaHuberJuma <katrina.huber@gmail.com>
2019-09-30 12:32:58 -0700

    initial commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,10 @@
+public class FirstJavaProgram {
+	public static void main(String[] args){
+		String name = "Kat";
+		System.out.println("My name is " + name);
+		Integer age = 29;
+		String town = "Berkeley, CA";
+		System.out.println("I am " + age + " years old");
+		System.out.println("My hometown is " + town);
+	}
+}
---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    public static void main(String[] args) { 
+        System.out.println("Hello Java World");
+    }
+}
+
+// Mandatory Method: Java program processing starts from the main() 
+// 	method which is a mandatory part of every executable Java program. 
+// 	It should look like this:
+// 			public static void main(String[] args){
+// 			    // your code
+// 			}
+
+
+// void is the return type when the method doesn't have a return
+// system is a class, .out creates an instance of a PrintStream class, that has a .println method for printing to the terminal
+
+// Method Names: All method names should start with a lower case letter. 
+	// If there are multiple words in the method, 
+	// they should be written in lowerCamelCase. 
+	// Typically, we use verbs as method names, 
+	// rather than noun statements.
