800dd630facf59dbe3558b26767717c082fa8e10
KatrinaHuberJuma <katrina.huber@gmail.com>
2019-10-01 08:36:42 -0700

    further Java practice

---
+++ basic13/Basic13Test.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Basic13Test {
+    public static void main(String[] args){
+        Basic13 obj = new Basic13();
+        // obj.printOneto255();
+        // obj.printOdds1to255();
+        // obj.printSumto255();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(10);
+        arr.add(15);
+        arr.add(2);
+        arr.add(25);
+        // obj.itrArr(arr);
+    }
+}
